b633ed9c3fd88e8b85c26a340061d74cf9deae92
vectorwing <lucas.t.barcellos@gmail.com>
2019-09-02 16:08:15 -0300

    Add TaskTitle

--- src/components/PanelTaskList.js
+++ src/components/PanelTaskList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import PanelTitle from './PanelTitle.js';
 import Task from './Task.js';
 import './PanelTaskList.css';
 
 function PanelTaskList() {
   return (
     <div className="panel PanelTaskList">
+      <PanelTitle title="Task List"/>
       <Task text="Do the dishes" />
       <Task text="Vacuum the living room" />
       <Task text="Buy groceries" />
